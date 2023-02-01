69e8d857c0b745623d26465757b12d07a3747606
Thiseas Boulakis <thiseas16@gmail.com>
2023-02-01 16:44:10 +0100

    added update, and delete

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,14 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get 'tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
